259b9a5498e0388ab3c25ee94b944de6b63c08f0
Blake Wood Jr <blake@rewired.one>
2022-06-22 13:33:54 -0700

    updated colors

--- mobile2/lib/constants/colors.dart
+++ mobile2/lib/constants/colors.dart
@@ -2,4 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static const purple = Color(0xFFCB4EE8);
+  static const green = Color(0xFF9CEC5B);
+  static const red = Color(0xFFEA2E2E);
+  static const yellow = Color(0xFFFFD83D);
+  static const headers = Color(0xFFFFFFFF);
+  static const primary = Color(0xFFE7E7E7);
+  static const secondary = Color(0xFF8B8B8B);
+  static const inactive = Color(0xFF7C7C7C);
 }
